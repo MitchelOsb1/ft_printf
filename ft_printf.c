391c9bb7d66d6591856a1bd67000ac69e3819e43
Mitchel Osborne <mosborne@e1z3r3p21.42.us.org>
2018-01-16 20:19:22 -0800

    Update

--- ft_printf.c
+++ ft_printf.c
@@ -6,61 +6,104 @@
 /*   By: mosborne <mosborne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/03 20:02:54 by mosborne          #+#    #+#             */
-/*   Updated: 2018/01/12 16:48:03 by mosborne         ###   ########.fr       */
+/*   Updated: 2018/01/16 19:09:41 by mosborne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
-int	is_valid(char *str)
+void	init_tools(t_tools *format)
 {
-	static int i = 0;
-
-	if (str[i] == '%')
-		i++;
-	if (is_digit(str[i]))
-		return (1);
-	if (str[i] == CONV(str[i]))
-		return (1);
-	if (str[i] == FLAG(str[i]))
-	{
-		i++;
-		if (is_digit(str[i]))
-			return (1);
-	}
-	if (str[i] == '%')
-		return (1);
+	format->minus = false;
+	format->plus = false;
+	format->space = false;
+	format->hash = false;
+	format->zero = false;
+	format->ifprec = false;
+	format->display = true;
+	format->star = false;
+	format->type = 0;
+	format->width = 0;
+	format->precision = 0;
+	format->argument = 0;
+	format->ret = 0;
+	format->len = 0;
 }
 
-char	*parse_form(char *str)
+void	set_flags(char c, char *ret, int x)
 {
-	while ()
+	struct s_tools i;
+
+	if (c == '-')
+		i.minus=true;
+	if (c == '0')
+		i.zero=true;
+	if (c == '+')
+		i.plus=true;
+	if (c == ' ')
+		i.space=true;
+	if (c == '#')
+		i.hash=true;
+}
+
+void	set_width(char c)
+{
+	struct s_tools i;
+
+	if (ft_isdigit(c))
+		i.width *= c; // fix decimal val
+}
+	
+void	set_prec(char *str, int x)
+{
+	if (str[x] == '.')
+		{
+			
+		}
+}
+
+char	*parse_form(char *str, char *ret, int x)
+{
+	t_tools		format;
+
+	init_tools(&format);
+	while ((str[x] != '\0') && ((str[x] == FLAG(str[x])) | (str[x] == CONV(str[x])) |
+	 (str[x] == MOD(str[x])) | (ft_isdigit(str[x]) == 1) | (str[x] == '.')))
 	{
-		if (str[i] == '')
+		set_flags(str[x]);
+		set_width(str[x]); // Pass va_list 
+		set_prec(str, x); // pass va_list
+		x++;
 	}
+	return (str);
 }
 
 int	ft_printf(char const *restrict str, ...)
 {
 	char		*ret;
+	static int	i = -1;
 	static int	x = 0;
 	va_list		input;
 
 	va_start(input, str);
-	while(str[x])
+	while (str[x])
 	{
-		if (*str == '%' && is_valid(str[x]))
-		{
-			ret = parse_form(str);
-		}
+		if (str[x] == '%')
+			ret = parse_form((char *)str, x);
+		if (str[x] == ',')
+			print_arg(ret, str, x);
 		x++;
 	}
+	printf("%5.1s\n", ret);
 	va_end(input);
-	return (ret);
+	return (1);
 }
 
 int	main(void)
 {
-	ft_printf("%s", "Hello");
-	printf("%s", "hello\n");
+	ft_printf("%s: %s", "HeY", "Hello");
+	printf("\n%s", "hello\n");
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: mosborne <mosborne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/03 20:02:57 by mosborne          #+#    #+#             */
-/*   Updated: 2018/01/12 16:48:20 by mosborne         ###   ########.fr       */
+/*   Updated: 2018/01/16 19:11:07 by mosborne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include "libft/libft.h"
 # include <stdio.h>
 # include <stdarg.h>
 # include <stdbool.h>
@@ -38,6 +39,8 @@ typedef struct  s_tools
     bool    space;
     bool    hash;
     bool    ifprec;
+    bool    display;
+    bool    star;
     char    type;
     int     width;
     int     precision;
@@ -45,12 +48,6 @@ typedef struct  s_tools
     int     ret;
     int     len;
 }               t_tools;
-
-struct conversion[] = {
-    {'s', &func},
-    {'S', &func},
-    
-}
 
 /* Spacing Flags */
 # define FLAG(c) (c == '-' || c == '0' || c == '+' || c == ' ' || c == '#')
@@ -61,6 +58,40 @@ struct conversion[] = {
     c == 'c' || c == 'C')
 
 /* Modifier Flags */
-# define MOD(c) (c == 'h' || c == 'l' || c == 'j' || c == 'z')
+# define MOD(c) (c == 'h' || c == 'l' || c == 'j' || c == 'z' || c == 'hh' || c == 'll')
 
 #endif
+
+/*
+	while (f[*i] != '\0' && (f[*i] == '.' || f[*i] == '-' || f[*i] == '+' ||
+			f[*i] == ' ' || f[*i] == '#' || f[*i] == '0' || f[*i] == '*' ||
+			f[*i] == 'h' || f[*i] == 'l' || f[*i] == 'j' || f[*i] == 'z' ||
+			f[*i] == 'q' || (f[*i] >= '0' && f[*i] <= '9')))
+
+
+** argument prototype: %[flags][width/margin][.precision][hh|h|l|ll|j|z]type
+**
+** flags:			'+' || '-' || ' ' || '#' || '0';
+**
+** width/margin:	min amount of displayed digits, does not cut digits if less,
+**					fill with spaces on the left side;
+**					if '*' added it's necessary to pass one more parameter with
+**					a number of spaces before the actual parameter;
+**
+** .precision:		for diouxX min amount of digits, fill with 0 is necessary;
+**					for eEf amount of digits after dot;
+**					for gG amount of all digits, if bigger displays number;
+**					for sS max amount of characters;
+**					if '*' added is necessary to pass one more parameter with a
+**					number of spaces before the actual parameter;
+**
+** hh|h|l|ll|j|z|q:	default values' modification:
+**						hh:	for idouxX - char;
+**						h:	for idouxX - short int;
+**						l:	for idouxX - long int, for feE - double;
+**						ll:	for idouxX - long long int;
+**						j:	for idouxX - intmax_t;
+**						z:	for idouxX - size_t;
+**						q:	for CS - prints Unicode
+
+*/
